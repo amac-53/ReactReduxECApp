a50e1b53bfc28344009cdb8bb9052e8886b90dcd
amane <amachonchon@gmail.com>
2023-02-08 15:50:30 +0900

    made stores, reducers, and set routing up

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import createStore from './reducks/store/store';
+import * as History from 'history';
+import { ConnectedRouter } from 'connected-react-router';
+
+const history = History.createBrowserHistory();
+export const store = createStore(history);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/templates/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <h2>
+            へい
+        </h2>
+    )
+}
+
+export default Home;
